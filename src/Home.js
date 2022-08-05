19c8d3456f2db820c8ba50c0c6a2a15f3d1085ef
Konrad Starczak <k.starczak@gmail.com>
2022-08-05 00:06:32 -0400

    Add bloglist component and add transfer blog data to json file

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
-    
-    return ( <div className="home">
-        <h2>Homepage</h2>
-    </div> );
+    const title = 'All Blog Posts';
+    const [blogs, setblogs] = useState(null)
+    const [isLoading, setisLoading] = useState(true);
+
+
+    const loadPage = (data) => {
+        setisLoading(false);
+        setblogs(data);
+    }
+        
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error('Could not fetch blog data! Try again in a few minutes or send an email k.starczak@mail.com if error persists');
+                    }
+                    return response.json()
+                })
+                .then(data => loadPage(data))
+                .catch(err => console.log(err.message));
+        }, 2000);
+    }, []);
+
+    return (
+        <div className="home">
+            {isLoading && <div>loading blogs...</div>}
+            {blogs && <Bloglist blogs={blogs} title={title} />}
+        </div>
+    );
 }
  
 export default Home;
